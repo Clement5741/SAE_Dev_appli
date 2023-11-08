fdc374bef6072495a00adef994725ce36403e5d0
ItsNezio <102726067+ItsNezio@users.noreply.github.com>
2023-11-08 14:32:15 +0100

    Maquette page hors connexion

--- SAE_Touiteur/src/HTML/page_base.php
+++ SAE_Touiteur/src/HTML/page_base.php
@@ -3,38 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <title>Accueil</title>
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/page_base.css">
 </head>
 <body>
 <div id="grid-container">
     <div id='Menu'>
-        <header>
             <div class='PartieMenu'>
                 <p>[emplacement du logo]</p>
             </div>
-        </header>
-        <section id='MilieuMenu'>
+
             <div class='PartieMenu'>
                 <p>Profil</p>
                 <p>Votre Mur</p>
+                <p>#Tags</p>
             </div>
-        </section>
-        <footer>
+
             <div class='PartieMenu'>
 <!--                <button href="../Compte/connexion.php" type="button">Connexion</button>-->
 <!--                <button href="../Compte/inscription.php" type="button">S'inscrire</button>-->
 <!--                <button href="../Compte/deconnexion.php" type="button">Se déconnecter</button>-->
                 <a href="../Compte/connexion.php"><div class="profile-button">Connexion</div></a>
                 <a href="../Compte/inscription.php"><div class="profile-button">S'inscrire</div></a>
-
                 <a href="#" onclick="return false"><div class="profile-button">Se déconnecter</div></a>
-
-
             </div>
-        </footer>
     </div>
+
     <div id='Touites'>
         <p>[emplacement des touites]</p>
+    </div>
+
+    <div id="tags_influencer">
+        <div id="tag">
+            <h1>#Tags</h1>
+        </div>
+        <div id="influencer">
+            <h1>#Influenceur</h1>
+        </div>
     </div>
 </div>
 </body>

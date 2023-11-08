fbe568feb09a30cbfad7c4e2125f041a7770fa12
Fiori4444 <florian.bortolotti88@gmail.com>
2023-11-09 00:30:30 +0100

    merge

---
+++ SAE_Touiteur/src/HTML/affichage_tags.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Liste des tags</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/affichage_tweet.css">
+</head>
+<body>
+<div class="tags-container">
+    <a href="fautmettreicilelienverslemurdetweet" class="back-button">&#8592;</a> <!---&#8592 represent the arrow-->
+    <div class="tweet-author">Nom d'utilisateur</div>
+
+</div>
+</body>
+</html>
